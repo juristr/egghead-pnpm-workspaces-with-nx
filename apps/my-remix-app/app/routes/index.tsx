9aeedc362a6ca15019c55236a5a4ab496e3a8cdb
Juri <juri.strumpflohner@gmail.com>
2022-08-15 22:20:46 +0200

    reuse logic

--- apps/my-remix-app/app/routes/index.tsx
+++ apps/my-remix-app/app/routes/index.tsx
@@ -1,32 +1,15 @@
+import { Button } from 'shared-ui';
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <Button
+        onClick={() => {
+          console.log('clicked!');
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 }
